de4d0812d37477141d81bbec0250b470f1eb09d1
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-09-05 23:40:13 +0300

    chore(backend): rename app to application

--- services/backend/application/main.py
+++ services/backend/application/main.py
@@ -1,15 +1,12 @@
-import settings
+import application.settings
 import os
 from waitress import serve
-from api.routes import app
+from application.api.routes import app
 
-def main():
+def start_server():
 	port = os.getenv('PORT', '5000')
 
 	if os.getenv('FLASK_ENV', 'development') == 'development':
 		app.run(debug=True, port=port)
 	else:
 		serve(app, host='0.0.0.0', port=port)
-
-if __name__ == '__main__':
-	main()
---
+++ services/backend/run.py
@@ -0,0 +1,4 @@
+from application.main import start_server
+
+if __name__ == '__main__':
+	start_server()
