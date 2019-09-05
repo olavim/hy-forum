962d006c8592c89e7731f53914c939da09eaaa07
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-09-05 20:29:50 +0300

    chore(ci): remove redundant install steps

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,9 +3,6 @@ set -eu
 
 # Install Heroku CLI
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sudo sh
-sudo mkdir -p /usr/local/lib /usr/local/bin
-sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
