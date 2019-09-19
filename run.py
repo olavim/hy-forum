e08c0b1ca29561416fe82174e6661e7172537210
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-09-19 21:46:28 +0300

    chore: ignore css folder

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import sass
 from application.main import start_server
 
@@ -5,9 +6,14 @@ sass_map = {'application/static/scss/default.scss': 'application/static/css/defa
 
 def compile_sass_to_css(sass_map):
 	print("Compiling scss to css:")
+
 	for source, dest in sass_map.items():
+		if not os.path.exists(os.path.dirname(dest)):
+			os.makedirs(os.path.dirname(dest))
+
 		with open(dest, 'w') as outfile:
 			outfile.write(sass.compile(filename=source, output_style='compressed'))
+
 		print('%s compiled to %s' % (source, dest))
 
 def build_styles():
