21206d7d5cecc4771a49244ec137575dabc07081
Olavi Mustanoja <tilastokeskus@gmail.com>
2019-09-28 13:45:54 +0300

    refactor(templates): no label in anchor

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-reloader_extra_files = ['application/static/scss/default.scss']
+reloader_extra_files = [
+	'application/static/scss/default.scss',
+	'application/static/scss/bootstrap-overrides.scss'
+]
 load_dotenv(find_dotenv())
 server_port = os.getenv('PORT', '5000')
 env = os.getenv('FLASK_ENV', 'development')
